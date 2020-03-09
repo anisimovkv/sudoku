c4618206760b270d565f3d064631b60ee0de92b3
Your Name <you@blae.com>
2020-03-09 14:18:31 +0200

    first commit

---
+++ src/sudoku.py
@@ -0,0 +1,53 @@
+from functools import reduce
+
+import numpy as np
+
+
+def sudoku_solver(input: np.ndarray) -> np.ndarray:
+    while np.any(input <= 0):
+        input = find_variant(input)
+    return input
+
+
+def find_variant(input: np.ndarray) -> np.ndarray:
+    all_elemnts: np.ndarray = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    for i in range(3):
+        start_row = i * 3
+        stop_row = start_row + 3
+        for j in range(3):
+            start_column = j * 3
+            stop_column = start_column + 3
+            block: np.ndarray = input[
+                                start_row:stop_row,
+                                start_column:stop_column]
+            for ii in range(start_row, stop_row):
+                for jj in range(start_column, stop_column):
+                    if input[ii, jj] != 0:
+                        continue
+                    row: np.ndarray = input[ii, :]
+                    colum: np.ndarray = input[:, jj]
+                    row_colum: np.ndarray = np.union1d(row, colum)
+                    result: np.ndarray = np.union1d(row_colum, block)
+                    values: np.ndarray = np.setdiff1d(all_elemnts, result)
+                    if values.size == 1:
+                        input[ii][jj] = values[0]
+    return input
+
+# colums: np.ndarray = np.transpose(
+#     input[:, start_column:stop_column])
+# for colum in colums:
+#     result = np.union1d(colum, rows.flatten())
+#     result = np.union1d(result, block.flatten())
+#     values = np.setdiff1d(all_elemnts, result)
+#     if values.size[0] == 1:
+#         input[] = values[0]
+
+# result: np.ndarray = reduce(
+#     np.intersect1d,
+#     (block.flatten(), colums.flatten(), rows.flatten()))
+
+# print(result)
+#     print('row', start_row, stop_row)
+#     print('colum', start_column, stop_column)
+#     print('end j')
+# print('end i')
---
+++ src/test_sudoku.py
@@ -0,0 +1,53 @@
+import unittest
+from typing import Tuple
+
+import numpy as np
+
+from .sudoku import sudoku_solver
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_sudoku_solver(self):
+        input, expect_output = self.init_data()
+        output = sudoku_solver(input.copy())
+        print(input)
+        print(output)
+        print(expect_output)
+        np.testing.assert_array_equal(expect_output, output)
+
+    @staticmethod
+    def init_data() -> Tuple:
+        # https://en.wikipedia.org/wiki/Sudoku
+        in_data: np.ndarray = np.array([
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9],
+        ])
+
+        out_data: np.ndarray = np.array([
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9],
+        ])
+        return in_data, out_data
+
+
+if __name__ == '__main__':
+    unittest.main()
